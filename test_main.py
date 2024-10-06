7d23ee6701c8dc1a10d8b4a457ebb4340b3ed2c7
Zachary Fennie <zachary.fennie@duke.edu>
2024-10-05 23:13:08 -0400

    testing

--- test_main.py
+++ test_main.py
@@ -1,3 +1,27 @@
 """
 TESTING FOR MAIN
 """
+
+import unittest
+from unittest.mock import patch
+from main import main
+
+
+class TestMainFunction(unittest.TestCase):
+    """Object for testing main with unittest"""
+
+    @patch("library.extract.extract")
+    @patch("library.transform_load.load")
+    @patch("library.query.query")
+    def test_main(self, mock_query, mock_load, mock_extract):
+        """Test the main function calls."""
+        main()
+
+        # Assert that each function was called once
+        mock_extract.assert_called_once()
+        mock_load.assert_called_once()
+        mock_query.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
