06a931fe06cf957ddde6248fe9cc1b0eae36715f
Zachary Fennie <zachary.fennie@duke.edu>
2024-10-05 23:32:55 -0400

    close

--- main.py
+++ main.py
@@ -7,16 +7,25 @@ from library.transform_load import load
 from library.query import query
 
 
+def main_results():
+    """Results function for testing main"""
+    results = {"extract": extract(), "transform": load(), "query": query()}
+    return results
+
+
 def main():
     """Main function for the ETL-Query script"""
-    # Extract
+    # extract
     extract()
 
-    # Transform and load
+    # transform and load
     load()
 
-    # Query
+    # query
     query()
+
+    # call results function for testing
+    main_results()
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -2,26 +2,16 @@
 TESTING FOR MAIN
 """
 
-import unittest
-from unittest.mock import patch
-from main import main
+from main import main_results
 
 
-class TestMainFunction(unittest.TestCase):
-    """Object for testing main with unittest"""
-
-    @patch("library.extract.extract")
-    @patch("library.transform_load.load")
-    @patch("library.query.query")
-    def test_main(self, mock_query, mock_load, mock_extract):
-        """Test the main function calls."""
-        main()
-
-        # Assert that each function was called once
-        mock_extract.assert_called_once()
-        mock_load.assert_called_once()
-        mock_query.assert_called_once()
+def test_func():
+    """Test function for main"""
+    test_dict = main_results()
+    assert test_dict["extract"] == "covid-geography/mmsa-icu-beds.csv"
+    assert test_dict["transform"] == "icuDB.db"
+    assert test_dict["query"] == "Success"
 
 
 if __name__ == "__main__":
-    unittest.main()
+    test_func()
